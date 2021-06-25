bc834228d88c6cf15678f4956ced770e13d87fab
William N. Green <wgreen@riotgames.com>
2021-06-25 12:51:52 -0700

    Add user to jupyter-users group

--- samlauthenticator/samlauthenticator.py
+++ samlauthenticator/samlauthenticator.py
@@ -638,7 +638,7 @@ class SAMLAuthenticator(Authenticator):
             if rid:
                 return not subprocess.call([self.create_system_user_binary, username])
             else:
-                return not subprocess.call([self.create_system_user_binary, username, '--uid ', rid])
+                return not subprocess.call([self.create_system_user_binary, username, '--uid ', rid, '-G jupyter-users'])
 
     def _check_username_and_add_user(self, username, rid = None):
         if self.validate_username(username) and \
